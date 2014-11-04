3d687c929831e65e52c8c2e4eb80466d0312727c
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2014-11-04 14:25:58 -0500

    add basic methods to ALEx

--- ALEx.java
+++ ALEx.java
@@ -8,20 +8,39 @@ public class ALEx{
 
 	private ArrayList<Item> items = new ArrayList<Item>();
 
-	public boolean moveTo(int x, int y) {
+	public boolean moveTo(int x, int y) { //move ALEx to position x, y 
+		this.x = x;
+		this.y = y; 
+
+		for(int i = 0; i < items.size(); i++) {
+			items.get(i).setX(this.x);
+			items.get(i).setY(this.y); 
+		}
 		return true; 
 	}
 
-	public boolean pickUp(Item item) {
+	public boolean pickUp(Item item) { //pick up item in environment 
 		return true; 
 	}
 
-	public boolean putDown(Item item) {
+	public boolean putDown(Item item) { //put down item in environment 
+		int i = hasItem(item); 
+		if(i != -1) {
+			items.get(i).setCarried(false); 
+			items.remove(i); 
+		}
+	}
+
+	public boolean lookForItem(Item item) { //look for an item in environment 
 		return true; 
 	}
 
-	public boolean lookForItem(Item item) {
-		return true; 
+	public int hasItem(Item item) { //check if ALEx is carrying an item
+		for(int i = 0; i < items.size(); i++) 
+			if(items.get(i).equals(item)) 
+				return i; 
+			
+		return -1; 
 	}
 
 	public static void main(String[] args) {
