ffbfa9067cc1b8e8b3e2e94805b4b44dd501a45c
Ruby M <ranmi.chaos.fox@gmail.com>
2014-11-04 20:56:51 -0500

    Better GUI

--- ALExGUI.java
+++ ALExGUI.java
@@ -1,5 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -7,6 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
@@ -25,7 +28,8 @@ public class ALExGUI {
 	GraphicsPanel grid;
 	JTextArea record; 
 	JTextField input;
-	JPanel righthalf; 
+	JPanel righthalf;
+	
 	int dimensions = 6; 
 	
 	public static void main(String[] args) {
@@ -36,13 +40,18 @@ public class ALExGUI {
 		
 		JFrame frame = new JFrame("ALEx - Artificial Linguistic Executor");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new BorderLayout());
+		frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.PAGE_AXIS));
 		
 		righthalf = new JPanel();
 		grid = new GraphicsPanel();
+		grid.setPreferredSize(new Dimension(500,500));
+		grid.setMaximumSize(new Dimension(500,500));
+		grid.setAlignmentX((float)0.5);
+		grid.setAlignmentY((float)0.5);
 		
-		frame.add(grid, BorderLayout.CENTER);
-		frame.add(righthalf, BorderLayout.EAST);
+		frame.add(grid);
+		Box.createRigidArea(new Dimension(0,5));
+		frame.add(righthalf);
 		
 		righthalf.setLayout(new BorderLayout());
 		
@@ -55,13 +64,13 @@ public class ALExGUI {
 		righthalf.add(scrollPane, BorderLayout.CENTER);
 		
 		input = new JTextField("Input");
-		righthalf.add(input, BorderLayout.SOUTH);
+		righthalf.add(input, BorderLayout.PAGE_END);
 		
 		t= new Timer(25,new TimerHandler());
 		t.start(); 
 		
 		//Show the frame
-		frame.setSize(900, 700);
+		frame.setSize(700, 700);
 		frame.setVisible(true);
 	}
 	
@@ -72,7 +81,7 @@ public class ALExGUI {
 		int columnwidth;
 		
 		public GraphicsPanel() {
-			graph = new BufferedImage(550, 550, BufferedImage.TYPE_INT_ARGB); 
+			graph = new BufferedImage(502, 502, BufferedImage.TYPE_INT_ARGB); 
 			graphdraw = graph.createGraphics(); 
 			columnwidth = Math.round((float)500/(float)dimensions);
 			System.out.println(columnwidth);
@@ -84,20 +93,22 @@ public class ALExGUI {
 		}
 		
 		public void paintComponent(Graphics g) {
-			graphdraw.setColor(Color.black);
 			
-			for(int i=1; i<=dimensions;i++){
+			graphdraw.setColor(Color.white);
+			graphdraw.fillRect(0, 0, 500, 500);
+			
+			graphdraw.setColor(Color.black);
+			for(int i=0; i<=dimensions;i++){
 				graphdraw.drawLine(i*columnwidth, 0, i*columnwidth, 500);
 			}
 			
-			for(int i=1; i<=dimensions;i++){
+			for(int i=0; i<=dimensions;i++){
 				graphdraw.drawLine(0, i*columnwidth, 500, i*columnwidth);
 			}
 			
 			g.drawImage(graph, 0, 0, null);
 		}
 
-		
 
 	}	
 
