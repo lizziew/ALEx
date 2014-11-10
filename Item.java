ad1bae2a5801c43100a5c5c0f6972dcc3790c947
Ruby M <ranmi.chaos.fox@gmail.com>
2014-11-10 13:40:49 -0500

    additional constructor

--- Item.java
+++ Item.java
@@ -11,6 +11,48 @@ public class Item {
 	private String shape; 
 	//private boolean edibility, heaviness; 	
 
+	public Item(int a, int b, int newcolor, int newshape){
+		x=a;
+		y=b;
+		isCarried = false;
+		
+		if (newcolor == 0){
+			color = "red";
+		}else if(newcolor == 1){
+			color = "orange";
+		}else if(newcolor == 2){
+			color = "yellow";
+		}else if(newcolor == 3){
+			color = "green";
+		}else if(newcolor == 4){
+			color = "blue";
+		}else if(newcolor == 5){
+			color = "lightblue";
+		}else if(newcolor == 6){
+			color = "purple";
+		}else if(newcolor == 7){
+			color = "pink";
+		}else if(newcolor == 8){
+			color = "brown";
+		}else if(newcolor == 9){
+			color = "gray";
+		}else if(newcolor == 10){
+			color = "black";
+		}
+		
+		if(newshape == 0){
+			shape = "circle";
+		}else if(newshape == 1){
+			shape = "moon";
+		}else if(newshape == 2){
+			shape = "square";
+		}else if(newshape == 3){
+			shape = "star";
+		}else if(newshape == 4){
+			shape = "triangle";
+		}
+	}
+	
 	public Item(int a, int b, String c, String s)
 	{
 		x = a;
